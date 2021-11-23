f37a477d878c87725b1bb271d5e4ad0944b7fd15
vjacheslav <devh@fiatum.com>
2021-11-23 10:13:15 +0300

    посмотрите все ветку пожалуйста

--- src/main/java/patterns/Main.java
+++ src/main/java/patterns/Main.java
@@ -6,11 +6,11 @@ import patterns.lesson3.factory.impl.TherapyFactory;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Лечение в терапии: ");
+        System.out.println("Лечение в терапии:");
         Application appTherapy = new Application(new TherapyFactory());
         appTherapy.treatment();
 
-        System.out.println("Лечение в хирургии: ");
+        System.out.println("Лечение в хирургии:");
         Application appSurgery = new Application(new SurgeryFactory());
         appSurgery.treatment();
     }
